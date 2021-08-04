496f4e18ec573e0ae7907e98c921bb537f34ecd3
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2021-08-04 14:48:06 +0900

    Add subscriber script.

--- sensor_sub.py
+++ sensor_sub.py
@@ -2,30 +2,27 @@
 # -*- coding: utf-8 -*- 
 import paho.mqtt.client as mqtt
 import config
-from time import sleep
  
 def on_connect(client, userdata, flag, rc):
   print("Connected with result code " + str(rc))
+  client.subscribe("hello/world") # Topic to subscribe
  
 def on_disconnect(client, userdata, flag, rc):
   if rc != 0:
      print("Unexpected disconnection.")
  
-def on_publish(client, userdata, mid):
-  print("publish: {0}".format(mid))
+def on_message(client, userdata, msg):
+  print("message: '{}' on topic {}".format(msg.payload, msg.topic))
  
 def main():
   client = mqtt.Client()
   client.on_connect = on_connect
   client.on_disconnect = on_disconnect
-  client.on_publish = on_publish
+  client.on_message = on_message
   client.username_pw_set(config.username, config.password)
   client.connect(config.broker, 1883, 60) 
  
-  client.loop_start()
-  while True:
-    client.publish("hello/world","Hello, World!")
-    sleep(3)
+  client.loop_forever()
  
 if __name__ == '__main__':
   main()
