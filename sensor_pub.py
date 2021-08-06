52708b58a763950ab8c8eb38bf51e3c5db4b4f6b
Yuki Kobayashi <kobayashi.yuki@gmail.com>
2021-08-07 00:16:51 +0900

    Add LED settings. Fix sensor measurement issue.

--- sensor_pub.py
+++ sensor_pub.py
@@ -7,6 +7,9 @@ from time import sleep
 
 # Sensors
 from bme280i2c import BME280I2C
+
+# LEDs
+import RPi.GPIO as GPIO
  
 def on_connect(client, userdata, flag, rc):
     print("Connected with result code " + str(rc))
@@ -19,6 +22,13 @@ def on_publish(client, userdata, mid):
     print("publish: {0}".format(mid))
  
 def main():
+    # LEDs
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.setup(18, GPIO.OUT)
+    GPIO.setup(22, GPIO.OUT)
+    GPIO.setup(27, GPIO.OUT)
+
     # Sensors
     bme280ch1 = BME280I2C(0x76)
     bme280ch2 = BME280I2C(0x77)
@@ -36,14 +46,27 @@ def main():
     client.on_publish = on_publish
     client.username_pw_set(config.username, config.password)
     client.connect(config.broker, 1883, 60) 
- 
+
     client.loop_start()
-    while True:
-        #client.publish("hello/world","Hello, World!")
-        client.publish("room1/temparature1","{:.1f}".format(bme280ch1.T))
-        client.publish("room1/pressure1","{:.1f}".format(bme280ch1.P))
-        client.publish("room1/humidity1","{:.1f}".format(bme280ch1.H))
-        sleep(3)
+    try:
+        GPIO.output(17, 1)
+        while True:
+            #client.publish("hello/world","Hello, World!")
+            r1 = bme280ch1.meas()
+            if not r1:
+                print('BME280 0x76 error')
+            client.publish("room1/temparature1","{:.1f}".format(bme280ch1.T))
+            client.publish("room1/pressure1","{:.1f}".format(bme280ch1.P))
+            client.publish("room1/humidity1","{:.1f}".format(bme280ch1.H))
+            GPIO.output(18, 1)
+            sleep(1)
+            GPIO.output(18, 0)
+            sleep(4)
+    except KeyboardInterrupt:
+        GPIO.cleanup(17)
+        GPIO.cleanup(18)
+        GPIO.cleanup(22)
+        GPIO.cleanup(27)
  
 if __name__ == '__main__':
     main()
